78b02f440daf010391a03faeb44a0a4bd5cd707f
Benjamin Feight <bjfeight@gmail.com>
2016-10-05 15:32:48 -0400

    Added Associations

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,2 +1,4 @@
 class Notice < ApplicationRecord
+  belongs_to :park
+  has_many :residents :through => :park
 end
--- app/models/park.rb
+++ app/models/park.rb
@@ -1,4 +1,6 @@
 class Park < ApplicationRecord
   has_many :users
   has_many :residents
+  has_many :notices
+  belongs_to :user 
 end
