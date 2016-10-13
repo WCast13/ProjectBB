c8ee5356beb57cbd6f8096936f58391673ca9980
Benjamin Feight <bjfeight@gmail.com>
2016-10-13 13:12:13 -0400

    Added Homepages for: Residents, notices, parks, rules

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :parks
   resources :users
 
+  get 'ParksHomepage' => 'application#parks_homepage'
+  get 'ResidentsHomepage' => 'application#residents_homepage'
+  get 'NoticesHomepage' => 'application#notices_homepage'
+  get 'RulesHomepage' => 'application#rules_homepage'
+  
   root 'users#homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
